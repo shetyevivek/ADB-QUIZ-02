7fb5e8f9fd61c4c947024d5b1d35bfeeb33c0a86
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-19 19:16:35 -0500

    cc

---
+++ A14a.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Answer 14a</title>
+</head>
+<body>
+  <div>
+    <h2>Student ID : 1001821620<br>Name : Vivek Vishwanath Shetye</h2>
+  </div>
+  </div>
+</body>
+</html>
+
+<?php
+//Start time
+$start = microtime(true);
+
+$con = new PDO("mysql:host=uta.cloud; dbname=vvs1620_database", "vvs1620_vivekshetye", "Shaleshwar12@");
+
+$years = $_POST['years'];
+$statec = $_POST['statec'];
+$ntimes = $_POST['ntimes'];
+
+$Memcache_file = "Memcache/index.cache.php";
+
+if (file_exists($Memcache_file) && filemtime($Memcache_file) > time() - 30)
+{
+    echo "From Cache:<br>";
+    include ($Memcache_file);
+}
+else
+{
+    for ($i = 0;$x < $ntimes; $i++)
+    {
+        $sql = "SELECT * FROM ptelect WHERE year=$year AND state_po='$stateco'";
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+        $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $str = "<table border='1'>";
+        $str .= "<tr><th>Candidate Name</th><th>Party Name</th><th>No. of Votes</th><th>State</th></tr>";
+        foreach ($arr as $list)
+        {
+            $str .= "<tr><td>" . $list['candidate'] . "</td><td>" . $list['party_detailed'] . "</td><td>" . $list['candidatevotes'] . "</td><td>" . $list['state'] . "</td></tr>";
+        }
+        $str .= "</table><br><br>";
+
+        $handle = fopen($Memcache_file, 'a');
+        fwrite($handle, $str);
+        fclose($handle);
+        echo "Cache Created<br/>";
+        echo $str;
+    }
+}
+
+//End time
+$end = microtime(true);
+echo "Query Time = " .round($end - $start, 4);
+?>
